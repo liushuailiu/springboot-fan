2263e7dbdefbb12973ec95938d41336d2df74c04
17799 <1779905848@qq.com>
2018-06-30 23:33:35 +0800

    springboot脚手架

--- src/main/java/com/example/springboot_day01/view/DemoController.java
+++ src/main/java/com/example/springboot_day01/view/DemoController.java
@@ -12,12 +12,23 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+
 /**
  * @author fly
  */
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+
+    /**
+     * 调用远程接口的api,
+     * 此接口为spring内置,
+     * 可以直接用来注入使用,
+     * 不需要配置,我们将通过 restTemplate
+     * 来调用第三方为我们提供的接口,
+     * 比如 支付宝/微信支付/天气接口...
+     */
 
     @Autowired
     RestTemplate restTemplate;
@@ -35,10 +46,28 @@ public class DemoController {
         throw new Exception();
     }
 
+
+    /**
+     * restTemplate 支持我们常用的请求类型,
+     * 比如 delete()/put(),但是通常情况下,
+     * 第三方接口是不会提供删除/添加功能的(除了我们自己系统内部间的调用),
+     * 所以一般只使用 get/post 请求, 根据返回内容的不同,
+     * rest 提供了以下几个方法 :
+     *      1.  getForEntity / postForEntity
+     *      2.  getForObject / postForObject
+     *      entity类的方法包含一个完整的请求头信息,比如浏览器信息,状态码,信息正文...
+     *      object类的方法只包含一个信息正文,在请求时可以提供一个信息正文的类型,
+     *      rest 将自动装箱,如果需要传入参数,则需要提供一个map集合,将参数装入map
+     *      进行传输(不建议直接写到url当中,比如 http://www.baidu.com?name=tom&pass=123 );
+     * @return
+     */
+
     @GetMapping("/entity")
     public Object restfulEntity(){
+        HashMap hashMap = new HashMap();
+        hashMap.put("name","tom");
         ResponseEntity<String> responseEntity =
-                restTemplate.getForEntity("http://gturnquist-quoters.cfapps.io/api/random",String.class);
+                restTemplate.getForEntity("http://gturnquist-quoters.cfapps.io/api/random",String.class,hashMap);
         responseEntity.getStatusCode();
         responseEntity.getHeaders();
         String string = responseEntity.getBody();
@@ -52,12 +81,32 @@ public class DemoController {
         return s;
     }
 
+    /**
+     * 文件上传的接口,与SSM没有区别,需要注意的是
+     * 页面上的from表单必须提供 enctype="multipart/form-data" 属性
+     * @param multipartFile 上传文件字节流对象
+     * @return
+     */
+
     @PostMapping("/upload")
     public Object upload(@RequestParam("file") MultipartFile multipartFile){
         String path = fileSystemStorageService.store(multipartFile);
         return path;
     }
 
+
+    /**
+     * 下载文件的接口,与SSM也没有区别,需要注意的是,
+     * 这既是一个下载接口,也是一个展示图片的接口(只适用于图片与视频),
+     * 如果你的请求地址为 : <a href="/demo/download?filename=2018-06-30/dd9cca9215154be0a5ce4045f74a1f97.PNG">下载</a>,
+     * 那么就会提供下载功能, 如果地址为 : <img src="/demo/download?filename=2018-06-30/dd9cca9215154be0a5ce4045f74a1f97.PNG">,
+     * 他就会自动展示图片(实际中应用非常广泛,这样做可以避免暴露服务器真实地址),
+     * 当然使用传统的通过虚拟路径的方式也可以访问到服务器图片,
+     * 但是因为springboot内置tomact,所以我们需要在配置文件中配置虚拟路径,
+     * 并且在拦截器中提供相应配置 见 : HandleConfig
+     * @param filename
+     * @return
+     */
     @GetMapping("/download")
     public ResponseEntity<Resource> download(String filename){
         Resource resource = fileSystemStorageService.loadAsResource(filename);
